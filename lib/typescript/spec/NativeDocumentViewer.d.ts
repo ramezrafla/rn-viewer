a51d5ccfe4e2a9aad8e6a477a0a5e46088f2494d
Ramez Rafla <ramez@classroomapp.com>
2025-01-20 17:21:42 -0500

    preparation for eas build

--- lib/typescript/spec/NativeDocumentViewer.d.ts
+++
@@ -1,6 +0,0 @@
-import type { TurboModule } from 'react-native';
-export interface Spec extends TurboModule {
-    viewDocument(bookmarkOrUri: string, permissions: string, mimeType?: string, title?: string, presentationStyle?: string): Promise<null>;
-}
-export declare const NativeDocumentViewer: Spec;
-//# sourceMappingURL=NativeDocumentViewer.d.ts.map
